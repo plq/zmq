5c79d85c2ac9b35460b6adc87d1d359c46a15208
Burak Arslan <burak.arslan@arskom.com.tr>
2013-01-19 22:07:06 +0200

    The code was re-licensed under the three-clause BSD license.
    Why on earth was it LGPL anyway?!

--- zmq.cpp
+++ zmq.cpp
@@ -1,38 +1,51 @@
 
 /*
- * zmq - A ZeroMQ Shell Wrapper. Copyright (C) Burak Arslan
+ * zmq - A ZeroMQ Shell Wrapper. Copyright (C) Arskom Ltd.
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the names of Arskom Ltd. or the ZeroMQ project, nor the
+ *       names of its employees or contributors may be used to endorse or
+ *       promote products derived from this software without specific prior
+ *       written permission.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  */
+
 
 #include <stdexcept>
 #include <iostream>
-using std::cout;
-using std::endl;
-
 #include <cstdlib>
 #include <cstring>
 
 #include <zmq.hpp>
+
+using std::cout;
+using std::endl;
+
 
 void usage() {
     cout << "Usage: \n"
          << "    zmq <SOCKET_TYPE> <URI> <MSG>\n"
          << "\n";
 
-    std::exit(0);
+    std::exit(1);
 }
 
 int get_socket_type(const char *str) {
@@ -50,6 +63,7 @@ int get_socket_type(const char *str) {
     return retval;
 }
 
+
 void recv_multipart(zmq::socket_t &socket) {
     int64_t more=-1;
     size_t more_size = sizeof (more);
@@ -57,12 +71,14 @@ void recv_multipart(zmq::socket_t &socket) {
         zmq::message_t response;
         socket.recv(&response);
 
-        std::string response_str((const char *)response.data(), response.size());
+        std::string response_str((const char *)response.data(),
+                                                             response.size());
         cout << response_str << endl;
 
         socket.getsockopt(ZMQ_RCVMORE, &more, &more_size);
     }
 }
+
 
 int main(int argc, char **argv) {
     if (argc < 3) {
